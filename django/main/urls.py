d4c9afda954f3e6ef1e790a4472bf08499701517
João Pedro Gama <joaocletto@gmail.com>
2023-10-23 20:31:43 -0300

    feature/fornecedor - desenvolvido crud de fornecedor

--- django/main/urls.py
+++ django/main/urls.py
@@ -1,6 +1,9 @@
+from supplier.urls import urlpatterns as supplier
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('supplier/', include(supplier)),
 ]
